ca5e15fe21d89f00eef1881c1d4bf275df8c90fd
WenJunZheng2001 <wenjunzheng2001@gmail.com>
2024-10-19 23:33:17 +0200

    added door animation

--- scripts/door.gd
+++ scripts/door.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var door_open = false
 
-
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready():
 	PuzzleManager.puzzle_trigger_event.connect(on_puzzle_trigger_escape_door)
@@ -11,25 +11,29 @@ func _ready():
 func on_puzzle_trigger_escape_door(is_puzzle_right: bool):
 	if is_puzzle_right && door_open == false:
 		PuzzleManager.is_door_animating = true
-		open_escape_door()
 		door_open = true
+		open_escape_door()
+		
 	elif is_puzzle_right == false && door_open == true:
 		PuzzleManager.is_door_animating = true
-		close_escape_door()
 		door_open = false
+		close_escape_door()
+		
 
 			
 func open_escape_door():	
-	var tween = create_tween()
-	position = Vector2(217, 71)
-	tween.tween_property(self, ^"position:y", 139, 0.5)
-	tween.tween_property(self, ^"position:x", 149, 0.5)
-	tween.tween_property(self, ^"position", Vector2(149, 139), 0.1)		
+	animated_sprite.play("door_opening")
+	#var tween = create_tween()
+	#position = Vector2(217, 71)
+	#tween.tween_property(self, ^"position:y", 139, 0.5)
+	#tween.tween_property(self, ^"position:x", 149, 0.5)
+	#tween.tween_property(self, ^"position", Vector2(149, 139), 0.1)		
 	
 	
 func close_escape_door():
-	var tween = create_tween()
-	position = Vector2(149, 139)
-	tween.tween_property(self, ^"position:x", 217, 0.5)
-	tween.tween_property(self, ^"position:y", 71, 0.5)
-	tween.tween_property(self, ^"position", Vector2(217, 71), 0.1)
+	animated_sprite.play_backwards("door_opening")
+	#var tween = create_tween()
+	#position = Vector2(149, 139)
+	#tween.tween_property(self, ^"position:x", 217, 0.5)
+	#tween.tween_property(self, ^"position:y", 71, 0.5)
+	#tween.tween_property(self, ^"position", Vector2(217, 71), 0.1)
