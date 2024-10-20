c4cb87f13959864187908f3f4d645ac35ce3b968
Franz <francesco.romeo-work@libero.it>
2024-10-20 16:37:11 +0200

    Enemy following player

---
+++ scenes/enemy.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export var speed: float = 20
+@export var view_range: float = 100
+@export var player: Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if position.distance_to(player.position) <= view_range:
+		_follow_player(delta)
+
+func _follow_player(delta):
+	var direction = (player.position - position).normalized()  # Direzione verso il giocatore
+	position += direction * speed * delta  # Imposta la velocità in base alla direzione
